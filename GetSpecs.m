b9b73af54a78d8218b2f461d21026dab8edae6ac
Kevin-Knudsen <s214376@dtu.dk>
2023-10-05 15:13:29 +0200

    Finished GetSpecs.asv

--- GetSpecs.m
+++ GetSpecs.m
@@ -7,20 +7,48 @@ input = GetNextLine(F);
 if input == "wav"
     % Get filename and it's sampling frequency
     filename = GetNextLine(F);
-    fs = GetNextLine(F);
+    fs = double(GetNextLine(F));
     GetNextLine(F);GetNextLine(F);GetNextLine(F);GetNextLine(F);GetNextLine(F);
 else
     % Get signal generator variables
     GetNextLine(F); GetNextLine(F);
     type = GetNextLine(F);
-    fs = GetNextLine(F);
-    duration = GetNextLine(F);
-    periodicity = GetNextLine(F);
-    frequencies = regexp(GetNextLine(F),', ','split');
-    frequencies = double(string(cell2mat(frequencies(:))));
+    fs = double(GetNextLine(F));
+    T_s = double(GetNextLine(F));
+    T_0 = double(GetNextLine(F));
+    freqs = regexp(GetNextLine(F),', ','split');
+    freqs = double(string(cell2mat(freqs(:))));
 end
 
-%Frequency analysis
+% Frequency analysis
+spectralResolution = double(GetNextLine(F));
+windowType = GetNextLine(F);
+overlap =double(GetNextLine(F));
+
+% Filtering
+responseType = GetNextLine(F);
+filterOrder = GetNextLine(F);
+ApproxMethod = GetNextLine(F);
+filterType = GetNextLine(F);
+fb = regexp(GetNextLine(F),', ','split');
+fb = double(string(cell2mat(fb(:))));
+
+% Plotting
+Xlim = zeros(2,1);
+Ylim = zeros(2,1);
+Xlim(1) = double(GetNextLine(F));
+Ylim(1) = double(GetNextLine(F));
+axisTypeX = GetNextLine(F);
+axisTypeY = GetNextLine(F);
+XlimCopy = GetNextLine(F);
+
+if XlimCopy == "nyquist"
+    Xlim(2) = fs/2;
+else
+    Xlim(2) = double(XlimCopy);
+end
+
+Ylim(2) = double(GetNextLine(F));
 
 fclose(F);
 
@@ -29,4 +57,5 @@ function L = GetNextLine(F)
     while L(1) == "%"
         L = fgetl(F);
     end
+    L = string(L);
 end
