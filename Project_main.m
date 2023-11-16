a5e1e77c1cb5967f1ff44e52bd33ebc5967a6026
Redhell272 <simon2ravn@gmail.com>
2023-11-16 17:19:22 +0100

    Better Main

--- Project_main.m
+++ Project_main.m
@@ -1,30 +1,43 @@
 
-type = "noise";
+clear;
+
+type = "tone-complex";
 fs = 20000;
-T_s = 3;
+T_s = 0.01;
 T_0 = 0.5;
-freqs = [5,7];
+freqs = [1000, 5000, 9000]; %[2500, 7500];
 responseType = "fir";
 approxMethod = "butterworth";
-filterOrder = 7;
-filterType = "lp";
-cutoff = [50];
+filterOrder = 5;
+filterType = "bp";
+cutoff = [2500, 7500];
 xLimFrequency = fs/2;
 
 run('Scripts/generateSignal.m');
 
-%run('Scripts/generateFilter.m');
+run('Scripts/generateFilter.m');
 
 figure('Name','Generated Signal')
 plot(time_vector, signal)
 
-%figure('Name', 'impz')
-%impz(b, a)
+figure('Name','Filtered Signal')
+plot(time_vector, output)
 
-%figure('Name', 'freqz')
-%freqz(b, a);
+FFT = fft(output);
+FFT = FFT/length(FFT);
 
-%figure('Name','Filtered Signal')
-%plot(time_vector, output)
+T0 = length(signal)/fs;
+delta_f = 1/T0;
 
+f_pos = 0:delta_f:fs/2;
+f_neg = -fs/2+delta_f:delta_f:-delta_f;
 
+freq_vector = [f_pos f_neg];
+
+figure('Name','Linear Signal Spectrum')
+subplot(2,1,1)
+plot(freq_vector, abs(FFT))
+xlim([0, fs/2])
+subplot(2,1,2)
+plot(freq_vector, angle(FFT))
+xlim([0, fs/2])
