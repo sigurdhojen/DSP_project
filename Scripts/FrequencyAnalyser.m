7b02f644a330c113fb9d60509f72baf8e04d91ad
Kevin-Knudsen <s214376@dtu.dk>
2023-11-19 15:26:31 +0100

    Fixed a bug in Frequency Analyser.m

--- Scripts/FrequencyAnalyser.m
+++ Scripts/FrequencyAnalyser.m
@@ -1,6 +1,7 @@
-Fs = 10e3;
-T = 0:1/Fs:4;
-Signal = vco(sin(2*pi*T),[0.1 0.4]*Fs,Fs);
+% Test signal
+% Fs = 10e3;
+% T = 0:1/Fs:4;
+% Signal = vco(sin(2*pi*T),[0.1 0.4]*Fs,Fs);
 
 NSignal = length(Signal);
 NWindow = floor(2*Fs/100); % 100 Hz spectral resolution for STFT
@@ -17,15 +18,19 @@ else
 end
 
 if WindowType == "hann"
-    FFTSignal = FFTSignal.*hann(length(Signal));
+    FFTSignal = FFTSignal.*hann(length(FFTSignal));
+    Window = hann(NWindow);
 elseif WindowType == "hamming"
-    FFTSignal = FFTSignal.*hamming(length(Signal));
+    FFTSignal = FFTSignal.*hamming(length(FFTSignal));
+    Window = hamming(NWindow);
+else
+    Window = rectwin(NWindow);
 end
 
 FFTSignal = fft(FFTSignal);
-%F = ( Fs/2/length(FFTSignal) ) : ( Fs/2/length(FFTSignal) ) : ( Fs/2 );
-%plot(F,20*log10(FFTSignal));
-%xlim([20 Fs/2]);
+% F = ( Fs/2/length(FFTSignal) ) : ( Fs/2/length(FFTSignal) ) : ( Fs/2 );
+% plot(F,20*log10(FFTSignal));
+% xlim([20 Fs/2]);
 
 [SpectrogramS, SpectrogramF, SpectrogramT] = ...
-        spectrogram(Signal,'yaxis',hann(NWindow),NOverlap,NFFT);
+        spectrogram(Signal,'yaxis',Window,NOverlap,NFFT);
